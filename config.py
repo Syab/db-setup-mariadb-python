e71266fdb5c92007fc8237735768c96e8288b76c
syab <syabster.phang18@gmail.com>
2020-03-30 21:39:37 +0800

    simple script to create table

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+dbhost  = os.environ['DBHOST']
+dbuser  = os.environ['DBUSER']
+dbpw    = os.environ['DBPW']
+dbport  = os.environ['DBPORT']
+dbname  = os.environ['DBNAME']
---
+++ createtable.py
@@ -0,0 +1,5 @@
+import time
+import logging
+import mysql.connector as mariadb
+from mysql.connector import errorcode
+from config import 
