e56e95c8f7a11510a7f5e3b0a54ff86e34b76472
syab <syabster.phang18@gmail.com>
2020-03-30 22:51:51 +0800

    uncommented fxn

--- createtable.py
+++ createtable.py
@@ -18,8 +18,8 @@ cursor = cnx.cursor()
 db_name = dbname
 
 TABLES = {}
-TABLES['test_url_store'] = (
-    "CREATE TABLE `test_url_store` ("
+TABLES['url_store'] = (
+    "CREATE TABLE `url_store` ("
     "_id int(11) AUTO_INCREMENT PRIMARY KEY,"
     "`long_url` text NOT NULL,"
     "`short_url` text NOT NULL,"
@@ -51,7 +51,7 @@ def create_tables():
                 print(err.msg)
 
 
-# check_db()
+check_db()
 create_tables()
 
 cursor.close()
